2eaf37e1874c1ab17ab7803aa741f3c7bf9f9349
leike <leike@meituan.com>
2017-12-29 10:53:04 +0800

    code save

--- app/final/service-worker.js
+++ app/final/service-worker.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+console.log('version1')
 var dataCacheName = 'weatherData-v1';
 var cacheName = 'weatherPWA-final-1';
 var filesToCache = [
